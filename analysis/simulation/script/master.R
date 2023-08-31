14dd5d3b889aee8e1061ed231871d55f59d3a890
Cyril Geismar <cg1521@dide.local>
2023-08-31 20:15:31 +0100

    cleaning the master.R to save individual RDS files to appropriate data folders -> Rich suggest doing so and then switching to duckdb if needed

--- analysis/simulation/script/master.R
+++ analysis/simulation/script/master.R
@@ -1,71 +1,129 @@
-library(randomNames)
+library(here)
+library(foreach)
+library(doFuture)
+
+dir.create(here("analysis/simulation/data", "scenarios"))
+dir.create(here("analysis/simulation/data", "simulations"))
+dir.create(here("analysis/simulation/data", "results"))
+dir.create(here("analysis/simulation/data", "logs"))
+
 
 # 0. Parameters --------------------------------------------------------------------------------------------
-n_scenarios <- 3000
-peak_coeffs <- seq(0.7 , 1, 0.1)
-
-# 1. Folder set-up --------------------------------------------------------------------------------------------
-folder_names <- list.dirs("analysis/simulation/data",
-    recursive = FALSE,
-    full.names = FALSE
-)
-run_name <- randomNames::randomNames(1, gender = 1, ethnicity = 6, which.names = "first")
-while (run_name %in% folder_names) {
-   run_name <- randomNames::randomNames(1, gender = 1, ethnicity = 6, which.names = "first")
-}
-output_dir <- paste0("analysis/simulation/data/", run_name)
-dir.create(output_dir, recursive = TRUE)
+n_scenarios <- 5
+n_simulations <- 10
+peak_coeffs <- seq(0.7, 1, 0.1)
+n_workers <- future::availableCores() - 1
+plan(multisession, workers = n_workers)
 
 time_start <- format(Sys.time(), "%Y-%m-%d-%H:%M")
 
-# 2. Generate scenarios --------------------------------------------------------------------------------------------
+
+# 1. Generate scenarios --------------------------------------------------------------------------------------------
 source("analysis/simulation/script/generate_scenarios.R")
 scenarios <- generate_scenarios(n_scenarios)
-saveRDS(scenarios, paste0(output_dir, "/scenarios.rds"))
+for (i in 1:n_scenarios) {
+    saveRDS(scenarios[[i]], file = paste0(here("analysis/simulation/data", "scenarios"), "/", scenarios[[i]]$scenario, ".rds"))
+}
 
-# 3. Run simulations -----------------------------------------------------------------------
-source("analysis/simulation/script/run_simulations.R")
-# simulations
-simulations_time <- system.time({
-    simulations <- run_simulations(scenarios)
+process_time <- system.time({
+
+  for (i in seq_along(scenarios)) {
+    scenario <- scenarios[[i]]
+
+    # Simulations
+    simulations <- foreach(
+      j = seq_len(n_simulations),
+      .options.future = list(seed = TRUE)
+    ) %dofuture% {
+      sim <- o2groups::simulate_groups(
+        duration = 100,
+        n_groups = scenario$n_groups,
+        size = scenario$size,
+        name = scenario$name,
+        delta = scenario$delta,
+        intro_n = scenario$intro_n,
+        r0 = scenario$r0,
+        generation_time = scenario$generation_time$d(0:50),
+        incubation_period = scenario$incubation_period$r(500)
+      )$data
+      sim$scenario <- scenario$scenario
+      sim$simulation <- j
+      return(sim)
+    }
+
+    # Results
+    results <-
+      foreach(
+        k = seq_along(simulations),
+        .options.future = list(seed = TRUE)
+      ) %dofuture% {
+        sim <- simulations[[k]]
+        peaks <- o2groups:::get_peak(sim)
+
+        result <- data.frame(
+          scenario = character(),
+          simulation = integer(),
+          peak_coeff = double(),
+          group = character(),
+          est = numeric(),
+          lower_ci = numeric(),
+          upper_ci = numeric(),
+          successes = numeric(),
+          trials = numeric(),
+          stringsAsFactors = FALSE
+        )
+
+        for (peak_coeff in peak_coeffs) {
+          for (group in sort(names(peaks))) {
+            d_group <- o2groups::early_delta(
+              data = sim,
+              min_t = 0,
+              max_t = peak_coeff * peaks[[group]],
+              size = scenario$size,
+              name = scenario$name
+            )
+
+            if (!group %in% rownames(d_group)) {
+              next
+            }
+
+            d_group <- d_group[group, ]
+            d_group <- as.data.frame(as.list(d_group))
+            d_group$group <- group
+            d_group$peak_coeff <- peak_coeff
+            d_group$simulation <- k
+            d_group$scenario <- scenario$scenario
+            result <- rbind(result, d_group)
+          }
+        }
+        return(result)
+      }
+
+    # Write RDS files to respective folders
+    saveRDS(simulations, file = paste0(here("analysis/simulation/data", "simulations"), "/", scenario$scenario, ".rds"))
+    saveRDS(results, file = paste0(here("analysis/simulation/data", "results"), "/", scenario$scenario, ".rds"))
+  }
 })
-saveRDS(simulations, file = paste0(output_dir, "/simulations.rds"))
-saveRDS(simulations_time, file = paste0(output_dir, "/simulations_time.rds"))
 
-# 4. Process simulations --------------------------------------------------------------------------------------------
-source("analysis/simulation/script/process_simulations.R")
-
-results_time <- system.time({
-    results <- process_simulations(scenarios,simulations, peak_coeffs)
-})
-
-saveRDS(results, file = paste0(output_dir, "/results.rds"))
-saveRDS(results_time, file = paste0(output_dir, "/results_time.rds"))
-
-
-# 4. Model Delta Bias --------------------------------------------------------------------------------------------
-#source("simulation/script/model.R")
 
 # 5. Save the run details --------------------------------------------------------------------------------------------
 time_end <- format(Sys.time(), "%Y-%m-%d-%H:%M")
-
-# Convert to POSIXct datetime objects
 time_start <- as.POSIXct(time_start, format = "%Y-%m-%d-%H:%M")
 time_end <- as.POSIXct(time_end, format = "%Y-%m-%d-%H:%M")
+total_time <- time_end - time_start
+scenarios_names <- lapply(scenarios, function(x) x$scenario)
 
-# Calculate the time difference
-totaltime <- time_end - time_start
-
-run_details <- data.frame(
-    run_name = run_name,
+log <- list(
     time_start = as.character(time_start),
     time_end = as.character(time_end),
-    totaltime = as.numeric(totaltime),
+    total_time = as.numeric(total_time),
+    process_time = process_time[[3]],
+    n_simulations = n_simulations,
     n_scenarios = n_scenarios,
     peak_coeffs = paste(peak_coeffs, collapse = ", "),
-    simulations_time = simulations_time[[3]],
-    results_time = results_time[[3]]
+    scenario_names = paste(scenarios_names, collapse = ", ")
 )
-saveRDS(run_details, file = paste0(output_dir, "/run_details.rds"))
 
+time_start <- format(time_start, "%Y-%m-%d-%H_%M")
+saveRDS(log, file = paste0(here("analysis/simulation/data", "logs"), "/log_", time_start, ".rds"))
 
